ef85ec56bf0df4653c8c42e9b99f6f7cf80fb2ab
shogo1108 <shogo1108@gmail.com>
2016-10-22 14:27:39 +0000

    1022_01

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,14 @@
+class NoticeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.sendmail_picture.subject
+  #
+  def sendmail_picture(picture)
+    @picture = picture
+
+    mail to: @user.email
+         subject: '【Picture】登録ありがとうございます'
+  end
+end
