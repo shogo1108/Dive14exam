bc1eaa06cc4ea3af9b493226459236021476db55
shogo1108 <shogo1108@gmail.com>
2016-11-20 02:30:44 +0000

    dive14exam_112002

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "herokuapp.com",
+    domain: "heroku.com",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
